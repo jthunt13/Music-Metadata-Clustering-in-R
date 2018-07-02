a1ad12f82bff4495934f8838619658e9d65d20e5
Jon Hunt <33164323+jthunt13@users.noreply.github.com>
2018-07-02 12:19:17 -0400

    Removed sensitive info

--- login.r
+++ login.r
@@ -5,7 +5,7 @@
 #------------------------------------------------------------------------------
 password <- # fill in here
 LocalMySQLLogin <- data.frame(user = "root",
-                              password = password,
-                              host = "127.0.0.1",
+                              password = ,
+                              host = "",
                               port = 3306,
                               stringsAsFactors = F)
